4243576d941001117b34c44786aa022e11759c6f
Joshua Haley <Joshua.haley@knights.ucf.edu>
2018-10-16 16:59:38 -0400

    Added Map, Static Overlays for specific route.

--- swagger_server/__init__.py
+++ swagger_server/__init__.py
@@ -1,15 +1,17 @@
-import connexion
+import connexion, os
 from flask import send_from_directory
+from flask_cors import CORS
 from swagger_server import encoder
+map_api_key = os.environ['MAPS_KEY']
 
-app = connexion.App(__name__, specification_dir='./swagger/', static_url_path='/static')
+app = connexion.App(__name__, specification_dir='./swagger/', )
 app.app.json_encoder = encoder.JSONEncoder
 app.add_api('swagger.yaml', arguments={'title': 'RandoTrack'})
-
+CORS(app.app)
 
 @app.route('/')
 def serve_homepage():
-    return send_from_directory('../static', 'index.html')
+    return send_from_directory('static', 'index.html')
 
 
 #app.run(port=80)
